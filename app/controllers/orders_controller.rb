0c27270c4364177edc8fe2afda5b1bb31fd498b5
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-24 17:36:47 +0900

    Remove comment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,7 +15,6 @@ class OrdersController < ApplicationController
       @order.save
       @order.update_total_quantity
     end
-    # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
     redirect_to orders_path
   end
 
